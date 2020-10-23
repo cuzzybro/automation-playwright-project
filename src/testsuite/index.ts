064ba745b5dc2a1dfcffc1d527a79037c5591913
LesJames <les.james@z.co.nz>
2020-10-23 18:34:37 +1300

    ignore .gitignore and screenshots. include screenshot to demo test case

--- src/testsuite/index.ts
+++ src/testsuite/index.ts
@@ -6,18 +6,20 @@ const playwright = require('playwright');
 (async() => {
 
 
-    const browser = await playwright.firefox.launch({headless:false})
+    const browser = await playwright.firefox.launch({headless:true})
     const context = await browser.newContext()
     
     const facebook = await context.newPage()
     let fb = new Facebook(context,facebook)
     await fb.launch()
+    await fb.page.screenshot({path: `screenshots/facebook.png`})
     await facebook.close()
 
     const instagram = await context.newPage()
     let insta = new Instagram(context, instagram)
     await insta.launch()
     await insta.loginPage.register()
+    await insta.page.screenshot({path: `screenshots/instagram.png`})
     await instagram.close()
 
     await browser.close()
